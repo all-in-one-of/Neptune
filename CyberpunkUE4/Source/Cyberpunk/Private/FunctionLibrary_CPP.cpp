7e9fe0ebfca61a7ed329c0818ecc6e274eddbe6f
Calvinatorr <Calvinbgood@hotmail.co.uk>
2019-01-27 16:34:13 +0000

    Added functions to C++ function library for editing reflection component cubemaps

--- CyberpunkUE4/Source/Cyberpunk/Private/FunctionLibrary_CPP.cpp
+++ CyberpunkUE4/Source/Cyberpunk/Private/FunctionLibrary_CPP.cpp
@@ -10,3 +10,28 @@ void UFunctionLibrary_CPP::SetSkyLightCubemapAngle(USkyLightComponent* Target, f
 		//skyLight->Update
 	}
 }
+
+void UFunctionLibrary_CPP::SetReflectionCaptureCubemap(UReflectionCaptureComponent* Target, UTextureCube* Cubemap)
+{
+	if (Target != nullptr && Cubemap != nullptr)
+	{
+		Target->Cubemap = Cubemap;
+
+		UWorld* World = Target->GetWorld();
+		Target->MarkDirtyForRecaptureOrUpload();
+		Target->UpdateReflectionCaptureContents(World, TEXT("Sky lighting updated - global cubemap re-captured"), false);
+		//Target->SetCaptureCompleted();
+	}
+}
+
+void UFunctionLibrary_CPP::SetReflectionCaptureCubemapAngle(UReflectionCaptureComponent* Target, float Angle)
+{
+	if (Target != nullptr)
+	{
+		Target->SourceCubemapAngle = Angle;
+
+		UWorld* World = Target->GetWorld();
+		Target->MarkDirtyForRecaptureOrUpload();
+		Target->UpdateReflectionCaptureContents(World, TEXT("Sky lighting updated - global cubemap re-captured"), false);
+	}
+}
--- CyberpunkUE4/Source/Cyberpunk/Public/FunctionLibrary_CPP.h
+++ CyberpunkUE4/Source/Cyberpunk/Public/FunctionLibrary_CPP.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "Runtime/Engine/Classes/Components/SkyLightComponent.h"
+#include "Runtime/Engine/Classes/Components/BoxReflectionCaptureComponent.h"
 
 #include "CoreMinimal.h"
 #include "Kismet/BlueprintFunctionLibrary.h"
@@ -19,4 +20,10 @@ class CYBERPUNK_API UFunctionLibrary_CPP : public UBlueprintFunctionLibrary
 
 	UFUNCTION(BlueprintCallable)
 		static void SetSkyLightCubemapAngle(USkyLightComponent* Target, float Angle);
+
+	UFUNCTION(BlueprintCallable)
+		static void SetReflectionCaptureCubemap(UReflectionCaptureComponent* Target, UTextureCube* Cubemap);
+
+	UFUNCTION(BlueprintCallable)
+		static void SetReflectionCaptureCubemapAngle(UReflectionCaptureComponent* Target, float Angle);
 };
